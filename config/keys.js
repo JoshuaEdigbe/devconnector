2049ba0ceaf70ed1aff2fa2d16fb5992b581598d
joshuaedigbe <joshuaedigbe@gmail.com>
2018-05-24 19:04:54 +0100

    Add JWTWeb Token to login auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://joshua:12345678@dbh15.mlab.com:27157/devconnector'
+  mongoURI: 'mongodb://joshua:12345678@dbh15.mlab.com:27157/devconnector',
+  secretOrKey: "secret"
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,81 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
-const bycrypt = require('bcryptjs');
-
+const gravatar = require("gravatar");
+const bycrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 // Load User Model
-const User = require('../../models/User')
-
+const User = require("../../models/User");
 
 // @route   GET api/users/test
 // @desc    Test post route
 // @access  Public
 
-router.get('/test', (req, res) => res.json({ msg: "user works" }));
-
+router.get("/test", (req, res) => res.json({ msg: "user works" }));
 
 // @route   GET api/users/register
 // @desc    Register post route
 // @access  Public
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   const { email, password, name } = req.body,
     avatar = gravatar.url(email, {
-      s: '200', //Size
-      r: 'pg', // Rating
-      d: 'mm' // Default
+      s: "200", //Size
+      r: "pg", // Rating
+      d: "mm" // Default
     });
 
   // Check if user exists
-  User.findOne({ email })
-    .then(user => {
-      if (user) return res.status(400).json({ email: "Email already exists" });
+  User.findOne({ email }).then(user => {
+    if (user) return res.status(400).json({ email: "Email already exists" });
 
-      const newUser = new User({
-        avatar,
-        email,
-        password,
-        name
-      });
+    const newUser = new User({
+      avatar,
+      email,
+      password,
+      name
+    });
 
-      // Encrypt Password and Save to DB
-      bycrypt.genSalt(10, (err, salt) => {
-        bycrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser.save()
-            .then(user => res.json(user))
-            .catch(err => console.log(err));
-        });
+    // Encrypt Password and Save to DB
+    bycrypt.genSalt(10, (err, salt) => {
+      bycrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err));
       });
     });
+  });
 });
-
 
 // @route   GET api/users/login
 // @desc    Login post route
 // @access  Public
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
 
-  const {email, password} = req.body;
+  User.findOne({ email }).then(user => {
+    //Check if user exist
+    if (!user) return res.status(400).json({ msg: "User not found" });
 
-  User.findOne({email})
-    .then(user => {
+    //Check if password is correct
+    bycrypt.compare(password, user.password).then(isMatch => {
+      if (!isMatch) return res.status(400).json({ msg: "Password not correct" });
 
-      //Check if user exist
-      if(!user) return res.status(400).json({msg: "User not found"});
-
-      //Check if password is correct
-      bycrypt.compare(password, user.password)
-        .then(isMatch => {
-          if(!isMatch) return res.status(400).json({msg: "Password not correct"});
-          return res.json({msg: "Login successful"});
+      // Sign Token
+      const payload = { ...user };
+      jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+        return res.json({
+          sucess: true,
+          token: "Bearer " + token
         });
-
+      });
     });
-
+  });
 });
 
 module.exports = router;
