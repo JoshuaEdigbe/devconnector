f8854a82110774f83765c59da53e7e569635a0e6
joshuaedigbe <joshuaedigbe@gmail.com>
2018-05-25 11:13:37 +0100

    Add PassportJS to protect routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bycrypt = require("bcryptjs");
+const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 
@@ -64,7 +65,8 @@ router.post("/login", (req, res) => {
 
     //Check if password is correct
     bycrypt.compare(password, user.password).then(isMatch => {
-      if (!isMatch) return res.status(400).json({ msg: "Password not correct" });
+      if (!isMatch)
+        return res.status(400).json({ msg: "Password not correct" });
 
       // Sign Token
       const payload = { ...user };
@@ -77,5 +79,17 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route   GET api/users/current
+// @desc    Test route authentication
+// @access  Private
+
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({ msg: "success" });
+  }
+);
 
 module.exports = router;
